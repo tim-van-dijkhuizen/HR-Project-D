483853178b248e0e364bd1167561ae612d315e5a
isabelle <0993377@hr.nl>
2021-05-12 13:01:09 +0200

    workout model

---
+++ Models/Workout.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FitbyteServer.Base;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace FitbyteServer.Models
+{
+    public class Workout
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool EnduranceTraining { get; set; }
+
+
+        public int Time { get; set; }
+
+   
+        public float Meter { get; set; }
+
+        public float AverageSpeed { get; set; }
+
+        public DateTime TimeAccomplished { get; set; }
+
+    
+    }
+}
