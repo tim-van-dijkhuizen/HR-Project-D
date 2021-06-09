891b728fb765a9484903152c83d54fbef7e7ac2b
isabelle <0993377@hr.nl>
2021-06-09 12:49:21 +0200

    profileservice update

--- Services/ProfileService.cs
+++ Services/ProfileService.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using FitbyteServer.Base;
 using FitbyteServer.Helpers;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace FitbyteServer.Services {
 
@@ -37,16 +40,54 @@ namespace FitbyteServer.Services {
         }
 
         public ConditionScores GetConditionScore(Genders gender, DateTime dateOfBirth, float distance) {
-            return ConditionScores.Medium;
+            var today = DateTime.Today;
+            // Calculate the age.
+            int age = today.Year - dateOfBirth.Year;
+            string test = "male.json";
+            if(gender == Genders.F)
+            {
+                test = "female.json";
+            }
+            StreamReader r = new StreamReader(test);
+            string jsonString = r.ReadToEnd();
+            JObject JsonCondition = JObject.Parse(jsonString);
+            List<Dictionary<string, object>> condition = JsonCondition.ToObject<List<Dictionary<string, object>>>();
+          
+            foreach(var con in condition)
+            {
+                if(int.Parse(con["minAge"].ToString()) >= age && int.Parse(con["maxAge"].ToString()) <= age)
+                {
+                    if(int.Parse(con["minDistance"].ToString()) >= distance && int.Parse(con["maxDistance"].ToString()) <= distance)
+                    {
+                        return (ConditionScores)Enum.Parse(typeof(ConditionScores), con["score"].ToString());
+                    }
+                    
+                }
+            }
+            return ConditionScores.None;
+
         }
 
         public Scheme GenerateScheme(Goals distanceGoal, int timeGoal, int daysAvailable, ConditionScores score) {
+            //StreamReader r = new StreamReader(workouts5kmPlus.json);
+            //string jsonString = r.ReadToEnd();
+            //JObject JsonCondition = JObject.Parse(jsonString);
+
+
+
             Scheme schema = new Scheme();
 
             schema.ConditionScore = score;
             schema.WorkoutsPerWeek = 3;
 
             schema.Workouts = new List<Workout>();
+
+            
+            schema.Workouts.Add(new EnduranceWorkout() { Title = $"Hardlopen 3,2km", Distance = 3.20f, Time = (timeGoal / 5) + 3 * 3 });
+            schema.Workouts.Add(new PowerWorkout() { Title = $"Krachttraining, 25 min" });
+
+
+            
 
             for(int i = 1; i <= 20; i++) {
                 schema.Workouts.Add(new EnduranceWorkout() {
